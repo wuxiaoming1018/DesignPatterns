c2f286b518aef790607c840be7107b226bc8f81f
Administrator <1398565099@qq.com>
2018-03-07 15:12:15 +0800

    设计模式_饿汉模式

--- src/singleton/hungry/Singleton.java
+++ src/singleton/hungry/Singleton.java
@@ -6,12 +6,14 @@ package singleton.hungry;
  */
 
 /**
- * 饿汉模式（无法向单例对象的构造方法传参）
+ * 饿汉模式（无法向单例对象的构造方法传参，最简单的一种单例模式，线程安全无法保证）
  */
 public class Singleton {
     private static final Singleton instance = new Singleton();
 
-    private Singleton(){}
+    private Singleton(){
+
+    }
 
     public static Singleton getInstance(){
         return instance;
